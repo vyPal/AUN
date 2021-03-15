9dd85ee434589fd02837e226a42a5fdd64aa7040
Jakub Palacký <kubik.palacky@gmail.com>
2021-03-15 06:26:27 +0000

    Made welcomer more extensible

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,10 +3,15 @@ const settings = require('discord-server-settings');
 module.exports = async (client, member) => {
   const guild = await member.guild;
   const uname = await member.user.id;
+  const guildname = await member.guild.name;
+  const membercount = await member.guild.memberCount;
   if(await settings.getSetting('welcomer_join', guild.id) == 'true'){
     const joinchannel = await settings.getSetting('welcomer_join_channel', guild.id)
     const channel = await guild.channels.cache.get(joinchannel)
     const joinmessage = await settings.getSetting('welcomer_join_msg', guild.id)
-    channel.send(joinmessage.replace("(member)", `<@${uname}>`))
+    joinmessage = joinmessage.replace("(member)", `<@${uname}>`)
+    joinmessage = joinmessage.replace("(server)", `<@${guildname}>`)
+    joinmessage = joinmessage.replace("(members)", `<@${membercount}>`)
+    channel.send(joinmessage)
   }
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,10 +3,15 @@ const settings = require('discord-server-settings');
 module.exports = async (client, member) => {
   const guild = await member.guild;
   const uname = await member.user.id;
+  const guildname = await member.guild.name;
+  const membercount = await member.guild.memberCount;
   if(await settings.getSetting('welcomer_leave', guild.id) == 'true'){
     const leavechannel = await settings.getSetting('welcomer_leave_channel', guild.id)
     const channel = await guild.channels.cache.get(leavechannel)
     const leavemessage = await settings.getSetting('welcomer_leave_msg', guild.id)
-    channel.send(leavemessage.replace("(member)", `<@${uname}>`))
+    leavemessage = leavemessage.replace("(member)", `<@${uname}>`)
+    leavemessage = leavemessage.replace("(server)", `<@${guildname}>`)
+    leavemessage = leavemessage.replace("(members)", `<@${membercount}>`)
+    channel.send(leavemessage)
   }
 }
